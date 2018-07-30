ad5302a3307d970965006d3aede2a47f6c86bc94
Emmanuel Ramos <emmanuel.ramos@dextra-sw.com>
2018-07-30 18:32:53 -0300

    Commands

---
+++ run-all.sh
@@ -0,0 +1,5 @@
+nohup java -jar yfwould-sd-goodweather-for-partytime/build/libs/sd-goodweather-partytime.jar
+nohup java -jar yfwould-gateway-goodweather-for-partytime/build/libs/gateway-goodweather-partytime.jar
+nohup java -jar yfwould-ms-spotify-partytime/build/libs/ms-spotify-partytime.jar
+nohup java -jar yfwould-ms-openweather-goodweather/build/libs/ms-openweather-goodweather.jar
+nohup java -jar yfwould-ms-goodweather-for-partytime/build/libs/ms-goodweather-partytime.jar
