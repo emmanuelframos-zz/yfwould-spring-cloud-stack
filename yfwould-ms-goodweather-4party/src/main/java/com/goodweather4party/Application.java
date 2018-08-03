29c3a25f2f72ee333c1a3c7836996e99a131afdc
Emmanuel Ramos <emmanuelframos@gmail.com>
2018-08-03 01:26:48 -0300

    Hystrix configuration

--- yfwould-ms-goodweather-4party/src/main/java/com/goodweather4party/Application.java
+++ yfwould-ms-goodweather-4party/src/main/java/com/goodweather4party/Application.java
@@ -2,14 +2,16 @@ package com.goodweather4party;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
